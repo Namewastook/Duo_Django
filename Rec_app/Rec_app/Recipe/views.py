59a09e2fd4c7946f53241064228bcd13ecfcd14c
TJ <philosophy.cheer@gmail.com>
2019-07-03 14:03:44 -0400

    API hits work, whole app needs styled

--- Rec_app/Rec_app/Recipe/views.py
+++ Rec_app/Rec_app/Recipe/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .models import FoodCombo, Seed_Db
 
 
-def testing(request):
+def main(request):
     if request.method == "POST":
 
         main = request.POST.get("main")
@@ -19,8 +19,7 @@ def testing(request):
             extras=extras,
             dishName=dishName
         )
-
-        new_combo.save()
+        # new_combo.save()
 
         response = eval(
             f'requests.get("https://api.edamam.com/search?q={dishName}&app_id=0890373f&app_key=5c5cb11c82dd77cd32e808aa96589367").json()')
@@ -34,14 +33,13 @@ def testing(request):
                 x += str(int(i.get('recipe').get('yield')))
                 x += " Calories: "
                 x += str(int(i.get('recipe').get('calories')))
-                for y in response["hits"]:
-                    a = " Contains: "
-                    a += str(int((y.get('recipe').get("totalNutrients").get("FAT").get("quantity"))))
-                    a += "g of Fat "
-                    a += str(int((y.get('recipe').get("totalNutrients").get("CHOCDF").get("quantity"))))
-                    a += "g of Carbs "
-                    a += str(int((y.get('recipe').get("totalNutrients").get("PROCNT").get("quantity"))))
-                    a += "g of Protein"
+                a = " Contains: "
+                a += str(int((i.get('recipe').get("totalNutrients").get("FAT").get("quantity"))))
+                a += "g of Fat "
+                a += str(int((i.get('recipe').get("totalNutrients").get("CHOCDF").get("quantity"))))
+                a += "g of Carbs "
+                a += str(int((i.get('recipe').get("totalNutrients").get("PROCNT").get("quantity"))))
+                a += "g of Protein"
 
                 meals.append(x)
                 meals.append(a)
@@ -51,6 +49,7 @@ def testing(request):
 
         context = {
             "response": meals
+
         }
 
         return render(request, "Recipe/recipe.html", context)
@@ -58,6 +57,13 @@ def testing(request):
 
 def seeding_db(request):
     if request.method == "POST":
+
+        a_list = []
+
+        main = ""
+        side = ""
+        extras = ""
+        dishName = ""
 
         main = request.POST.get("main")
         side = request.POST.get("side")
@@ -71,54 +77,47 @@ def seeding_db(request):
             dishName=dishName
         )
 
+        a_list.append(new_combo)
         new_combo.save()
+
+    meals = []
 
     response = eval(
         f'requests.get("https://api.edamam.com/search?q={dishName}&app_id=0890373f&app_key=5c5cb11c82dd77cd32e808aa96589367").json()')
 
-    rec_label = []
-    rec_yield = []
-    rec_calories = []
-    rec_fat_quanity = []
-    rec_carbs_quanity = []
-    rec_protein_quanity = []
+    rec_label = ""
+    rec_yield = ""
+    rec_calories = ""
+    rec_fat_quanity = ""
+    rec_carbs_quanity = ""
+    rec_protein_quanity = ""
 
     for i in response['hits']:
-        x = (i.get('recipe').get('label'))
-        y = str(int(i.get('recipe').get('yield')))
-        z = str(int(i.get('recipe').get('calories')))
-        for y in response["hits"]:
-            uno = str(
-                int((y.get('recipe').get("totalNutrients").get("FAT").get("quantity"))))
-            dos = str(
-                int((y.get('recipe').get("totalNutrients").get("CHOCDF").get("quantity"))))
-            tres = str(
-                int((y.get('recipe').get("totalNutrients").get("PROCNT").get("quantity"))))
+        rec_label = (i.get('recipe').get('label'))
+        rec_yield = str(int(i.get('recipe').get('yield')))
+        rec_calories = str(int(i.get('recipe').get('calories')))
+        rec_fat_quanity = str(
+            int((i.get('recipe').get("totalNutrients").get("FAT").get("quantity"))))
+        rec_carbs_quanity = str(
+            int((i.get('recipe').get("totalNutrients").get("CHOCDF").get("quantity"))))
+        rec_protein_quanity = str(
+            int((i.get('recipe').get("totalNutrients").get("PROCNT").get("quantity"))))
 
-    rec_label.append(x)
-    rec_yield.append(y)
-    rec_calories.append(z)
-    rec_fat_quanity.append(uno)
-    rec_carbs_quanity.append(dos)
-    rec_protein_quanity.append(tres)
+        seeded_db = Seed_Db(
+            rec_label=rec_label,
+            rec_yield=rec_yield,
+            rec_calories=rec_calories,
+            rec_fat_quanity=rec_fat_quanity,
+            rec_carbs_quanity=rec_carbs_quanity,
+            rec_protein_quanity=rec_protein_quanity,
+            foodcombo=new_combo
+        )
 
-    seeded_db = Seed_Db(
-        rec_label=rec_label,
-        rec_yield=rec_yield,
-        rec_calories=rec_calories,
-        rec_fat_quanity=rec_fat_quanity,
-        rec_carbs_quanity=rec_carbs_quanity,
-        rec_protein_quanity=rec_protein_quanity,
-        foodcombo=new_combo
-    )
+        meals.append(seeded_db)
+        seeded_db.save()
 
-    seeded_db.save()
+    context = {
+        "response": meals
+    }
 
-    return HttpResponse("It worked?")
-
-
-# def another_view(request):
-
-#     Seed_Db()
-
-#     return render(request, "Recipe/recipe.html", context)
+    return render(request, "Recipe/recipe.html", context)
